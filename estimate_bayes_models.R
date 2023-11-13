27fd4f9eb7f894d7b5206bceab63ad1b6851b483
Andreas Widmann <widmann@uni-leipzig.de>
2023-11-13 11:24:37 +0100

    100k iterations

--- estimate_bayes_models.R
+++ estimate_bayes_models.R
@@ -10,7 +10,7 @@ m_reduced_bayes <- brm(m_reduced, data = dat, cores = 4, iter = 10000, save_pars
 
 #### SNR x cond ----
 m1 <- pa ~ snr_ctr * cond + (1 + snr_ctr * cond + trial_ctr + I(trial_ctr ^ 2) | subj)
-fm1_bayes <- brm(m1, data = dat, cores = 4, iter = 10000, save_pars = save_pars(all = TRUE))
+fm1_bayes <- brm(m1, data = dat, cores = 4, iter = 25000, save_pars = save_pars(all = TRUE))
 
 # Warnmeldungen:
 #   1: There were 6 transitions after warmup that exceeded the maximum treedepth. Increase max_treedepth above 10. See
@@ -24,7 +24,7 @@ summary(fm1_bayes)
 
 #### SNR x cond + trial_ctr x cond ----
 m2 <- pa ~ snr_ctr * cond + trial_ctr * cond + I(trial_ctr ^ 2) * cond + (1 + snr_ctr * cond + trial_ctr + I(trial_ctr ^ 2) | subj)
-fm2_bayes <- brm(m2, data = dat, cores = 4, iter = 10000, save_pars = save_pars(all = TRUE))
+fm2_bayes <- brm(m2, data = dat, cores = 4, iter = 25000, save_pars = save_pars(all = TRUE))
 
 summary(fm2_bayes)
 # plot(fm2_bayes)
@@ -33,7 +33,7 @@ summary(fm2_bayes)
 
 #### SNR x cond x trial_ctr ----
 m3 <- pa ~ snr_ctr * cond * trial_ctr + I(trial_ctr ^ 2) * cond + (1 + snr_ctr * cond + trial_ctr + I(trial_ctr ^ 2) | subj)
-fm3_bayes <- brm(m3, data = dat, cores = 4, iter = 10000, save_pars = save_pars(all = TRUE))
+fm3_bayes <- brm(m3, data = dat, cores = 4, iter = 25000, save_pars = save_pars(all = TRUE))
 
 summary(fm3_bayes)
 # plot(fm3_bayes)
@@ -62,16 +62,27 @@ fm1_bayes <- add_criterion(fm1_bayes, criterion = c("waic"))
 fm2_bayes <- add_criterion(fm2_bayes, criterion = c("waic"))
 fm3_bayes <- add_criterion(fm3_bayes, criterion = c("waic"))
 
-# save(file = "bayes_models.Rdata", fm1_bayes, fm2_bayes, fm3_bayes, fm3_fd_bayes, fm3_sgdi_bayes, m_reduced_bayes)
-save(file = "bayes_models.Rdata", fm1_bayes, fm2_bayes, fm3_bayes, m_reduced_bayes)
+save(file = "bayes_models.Rdata", fm1_bayes, fm2_bayes, fm3_bayes, fm3_fd_bayes, fm3_sgdi_bayes, m_reduced_bayes)
+# save(file = "bayes_models.Rdata", fm1_bayes, fm2_bayes, fm3_bayes, m_reduced_bayes)
 
-  #### Compare models
+#### Compare models
 # loo_compare(fm1_bayes, fm2_bayes, fm3_bayes)
-waic(fm2_bayes, fm3_bayes)
+waic(fm2_bayes, fm1_bayes)
+waic(fm3_bayes, fm2_bayes)
 
-mw <- model_weights(fm1_bayes, fm2_bayes, fm3_bayes)
+# mw <- model_weights(fm1_bayes, fm2_bayes, fm3_bayes)
+mw <- model_weights(fm2_bayes, fm3_bayes)
 mw
-mw[3]/mw[2]
+mw[2]/mw[1]
 
-bayes_factor(fm2_bayes,fm1_bayes)
-bayes_factor(fm3_bayes,fm2_bayes)
+bayes_factor(fm2_bayes, fm1_bayes)
+set.seed(1)
+bayes_factor(fm3_bayes, fm2_bayes)
+
+bf_32 <- list()
+for(seed in 0:99) {
+  print(seed)
+  set.seed(seed)
+  bf_32[[seed + 1]] <- bayes_factor(fm3_bayes, fm2_bayes, maxiter = 1000)
+}
+save(file = "bf_1k.Rdata", bf_32)
