56975929b7f9a82425982f2b01a579ce3d2fc6bf
Andreas Widmann <widmann@uni-leipzig.de>
2024-03-19 18:47:07 +0100

    Code cleanup

--- estimate_bayes_models.R
+++ estimate_bayes_models.R
@@ -1,90 +1,59 @@
+# This script reproduces the Bayesian linear mixed models of the manuscript
+# "Pupillometry is sensitive to speech masking during story listening: The
+# critical role of modeling temporal trends" by Andreas Widmann, Björn Herrmann,
+# and Florian Scharf.
+#
+# Authors: Florian Scharf, florian.scharf@uni-kassel.de and Andreas Widmann, widmann@uni-leipzig.de
+# Copyright (c) 2024 Florian Scharf, University of Kassel and Andreas Widmann, Leipzig University
+
 library(brms)
-library(loo)
+
+rm(list = ls())
+options(scipen = 4, width = 100)
 options(mc.cores = 4)
 
 load(file = "data.Rdata")
 
-contrasts(dat$cond) <- contr.sum(2)
-
-# cond x trial_ctr
+#### cond x trial_ctr
 m_reduced <- pa ~ trial_ctr * cond + I(trial_ctr ^ 2) * cond + (1 + cond * trial_ctr + cond * I(trial_ctr ^ 2) | subj)
 m_reduced_bayes <- brm(m_reduced, data = dat, cores = 4, iter = 10000, save_pars = save_pars(all = TRUE))
+
+summary(m_reduced_bayes)
+plot(m_reduced_bayes)
+pp_check(m_reduced_bayes, ndraws = 100)
 
 #### SNR x cond ----
 m1 <- pa ~ snr_ctr * cond + (1 + snr_ctr * cond + trial_ctr + I(trial_ctr ^ 2) | subj)
 fm1_bayes <- brm(m1, data = dat, cores = 4, iter = 25000, save_pars = save_pars(all = TRUE))
 
-# Warnmeldungen:
-#   1: There were 6 transitions after warmup that exceeded the maximum treedepth. Increase max_treedepth above 10. See
-# https://mc-stan.org/misc/warnings.html#maximum-treedepth-exceeded 
-# 2: Examine the pairs() plot to diagnose sampling problems
-
 summary(fm1_bayes)
-# plot(fm1_bayes)
-# pp_check(fm1_bayes, ndraws = 100)
-
+plot(fm1_bayes)
+pp_check(fm1_bayes, ndraws = 100)
 
 #### SNR x cond + trial_ctr x cond ----
 m2 <- pa ~ snr_ctr * cond + trial_ctr * cond + I(trial_ctr ^ 2) * cond + (1 + snr_ctr * cond + trial_ctr + I(trial_ctr ^ 2) | subj)
 fm2_bayes <- brm(m2, data = dat, cores = 4, iter = 25000, save_pars = save_pars(all = TRUE))
 
 summary(fm2_bayes)
-# plot(fm2_bayes)
-# pp_check(fm2_bayes, ndraws = 100)
-
+plot(fm2_bayes)
+pp_check(fm2_bayes, ndraws = 100)
 
 #### SNR x cond x trial_ctr ----
 m3 <- pa ~ snr_ctr * cond * trial_ctr + I(trial_ctr ^ 2) * cond + (1 + snr_ctr * cond + trial_ctr + I(trial_ctr ^ 2) | subj)
 fm3_bayes <- brm(m3, data = dat, cores = 4, iter = 25000, save_pars = save_pars(all = TRUE))
 
 summary(fm3_bayes)
-# plot(fm3_bayes)
-# pp_check(fm3_bayes, ndraws = 100)
-
+plot(fm3_bayes)
+pp_check(fm3_bayes, ndraws = 100)
 
 #### Fixation duration: SNR x cond x trial_ctr ----
 m3_fd <- fd ~ snr_ctr * cond * trial_ctr + I(trial_ctr ^ 2) * cond + (1 + snr_ctr * cond + trial_ctr + I(trial_ctr ^ 2) | subj)
 fm3_fd_bayes <- brm(m3_fd, data = dat, cores = 4, iter = 10000, save_pars = save_pars(all = TRUE))
+
 summary(fm3_fd_bayes)
+plot(fm3_fd_bayes)
+pp_check(fm3_fd_bayes, ndraws = 100)
 
-
-#### Spatial gaze dispersion: SNR x cond x trial_ctr ----
-m3_sgdi <- sgd_i ~ snr_ctr * cond * trial_ctr + I(trial_ctr ^ 2) * cond + (1 + snr_ctr * cond + trial_ctr + I(trial_ctr ^ 2) | subj)
-fm3_sgdi_bayes <- brm(m3_sgdi, data = dat, cores = 4, iter = 10000, save_pars = save_pars(all = TRUE))
-summary(fm3_sgdi_bayes)
-
-save(file = "bayes_models.Rdata", fm1_bayes, fm2_bayes, fm3_bayes, fm3_fd_bayes, fm3_sgdi_bayes, m_reduced_bayes)
-
-load(file = "bayes_models.Rdata")
-     
-# fm1_bayes <- add_criterion(fm1_bayes, criterion = c("loo", "waic"), reloo = T)
-# fm2_bayes <- add_criterion(fm2_bayes, criterion = c("loo", "waic"), reloo = T)
-# fm3_bayes <- add_criterion(fm3_bayes, criterion = c("loo", "waic"), reloo = T)
-fm1_bayes <- add_criterion(fm1_bayes, criterion = c("waic"))
-fm2_bayes <- add_criterion(fm2_bayes, criterion = c("waic"))
-fm3_bayes <- add_criterion(fm3_bayes, criterion = c("waic"))
-
-save(file = "bayes_models_effect_coding.Rdata", fm1_bayes, fm2_bayes, fm3_bayes, fm3_fd_bayes, fm3_sgdi_bayes, m_reduced_bayes)
-# save(file = "bayes_models.Rdata", fm1_bayes, fm2_bayes, fm3_bayes, m_reduced_bayes)
-
-#### Compare models
-# loo_compare(fm1_bayes, fm2_bayes, fm3_bayes)
-waic(fm2_bayes, fm1_bayes)
-waic(fm3_bayes, fm2_bayes)
-
-# mw <- model_weights(fm1_bayes, fm2_bayes, fm3_bayes)
-mw <- model_weights(fm2_bayes, fm3_bayes)
-mw
-mw[2]/mw[1]
-
-bayes_factor(fm2_bayes, fm1_bayes)
-set.seed(1)
-bayes_factor(fm3_bayes, fm2_bayes)
-
-bf_32 <- list()
-for(seed in 0:99) {
-  print(seed)
-  set.seed(seed)
-  bf_32[[seed + 1]] <- bayes_factor(fm3_bayes, fm2_bayes, maxiter = 1000)
-}
-save(file = "bf_1k.Rdata", bf_32)
+# Note that we did not upload the Bayesian LMMs to Github because the file size
+# was > 1 GB. The file is available on request from the authors.
+save(file = "bayes_models.Rdata", fm1_bayes, fm2_bayes, fm3_bayes, fm3_fd_bayes, m_reduced_bayes)
--- estimate_models.R
+++ estimate_models.R
@@ -1,3 +1,11 @@
+# This script reproduces the linear mixed models of the manuscript "Pupillometry
+# is sensitive to speech masking during story listening: The critical role of
+# modeling temporal trends" by Andreas Widmann, Björn Herrmann, and Florian
+# Scharf.
+# 
+# Authors: Andreas Widmann, widmann@uni-leipzig.de and Florian Scharf, florian.scharf@uni-kassel.de
+# Copyright (c) 2024 Andreas Widmann, Leipzig University and Florian Scharf, University of Kassel
+
 library(lmerTest)
 library(emmeans)
 library(MuMIn)
@@ -7,7 +15,7 @@ library(optimx)
 options(scipen = 4, width = 100)
 rm(list = ls())
 
-# Import data
+# Load data
 load("data.Rdata")
 
 #### SNR x cond ----
@@ -15,7 +23,7 @@ m1 <- pa ~ snr_ctr * cond + (1 + cond | subj )
 fm1 <- lmer(m1, data = dat, control = lmerControl(calc.derivs = F), REML = F)
 summary(fm1)
 r.squaredGLMM(fm1)
-tab_model(fm1, digits = 3)
+tab_model(fm1, digits = 1)
 
 #### SNR x cond + trial x cond ----
 # dat$cond = relevel(dat$cond, ref = "Intact" )
@@ -26,41 +34,7 @@ fm2 <- lmer(m2, data = dat,
             REML = F)
 summary(fm2)
 r.squaredGLMM(fm2)
-tab_model(fm2, digits = 3)
+tab_model(fm2, digits = 1)
+anova(fm1, fm2)
 
-#### SNR x trial x cond ----
-m3 <- pa ~ snr_ctr * trial_ctr * cond + I(trial_ctr ^ 2) * cond + (1 + cond + trial_ctr + I(trial_ctr ^ 2) | subj)
-fm3 <- lmer(m3, data = dat,
-            control = lmerControl(calc.derivs = F, optimizer ='optimx', optCtrl=list(method='nlminb')),
-            REML = F)
-summary(fm3)
-r.squaredGLMM(fm3)
-tab_model(fm3, digits = 3)
-
-anova(fm2, fm3)
-
-#### fixation duration: SNR x trial x cond ----
-m3_fd <- fd ~ snr_ctr * trial_ctr * cond + I(trial_ctr ^ 2) * cond + (1 + cond + trial_ctr + I(trial_ctr ^ 2) | subj)
-fm3_fd <- lmer(m3_fd, data = dat,
-            control = lmerControl(calc.derivs = F, optimizer ='optimx', optCtrl=list(method='nlminb')),
-            REML = F)
-summary(fm3_fd)
-r.squaredGLMM(fm3_fd)
-tab_model(fm3_fd, digits = 5)
-
-emt <- emtrends(fm3_fd, ~ cond + trial_ctr, var = "snr_ctr", at = list(trial_ctr=-10.5:10.5, snr_ctr=-2:2))
-emmeans(emt, ~ cond + trial_ctr)
-
-#### spatial gaze dispersion: SNR x trial x cond ----
-m3_sgdi <- sgd_i ~ snr_ctr * trial_ctr * cond + I(trial_ctr ^ 2) * cond + (1 + cond + trial_ctr + I(trial_ctr ^ 2) | subj)
-fm3_sgdi <- lmer(m3_sgdi, data = dat,
-               control = lmerControl(calc.derivs = F, optimizer ='optimx', optCtrl=list(method='nlminb')),
-               REML = F)
-summary(fm3_sgdi)
-r.squaredGLMM(fm3_sgdi)
-tab_model(fm3_sgdi, digits = 5)
-
-emt <- emtrends(fm3_sgdi, ~ cond + trial_ctr, var = "snr_ctr", at = list(trial_ctr=-10.5:10.5, snr_ctr=-2:2))
-emmeans(emt, ~ cond + trial_ctr)
-
-save(file = "models.Rdata", fm1, fm2, fm3, fm3_fd, fm3_sgdi)
+save(file = "models.Rdata", fm1, fm2)
